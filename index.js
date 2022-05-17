8e1135dfb8c61444e24ac8e87731fc89c8fdaa1a
Andrés Caffarena <andrescaffarena@hotmail.com>
2022-05-17 15:18:08 -0300

    funciona limitadamente en express

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
 const im= require('imagemagick');
 const uuid=require('uuid')
-const tmpfd=uuid.v4()
+const { exec } = require("child_process");
+const express = require('express')
+
 const fs=require("fs")
-const folder="./temp/" + tmpfd
-const filename="dd.pdf"
-const imagebase="dd.png"
-fs.mkdirSync(folder)
-im.convert([filename,'-colorspace', 'RGB', '-alpha' ,'remove' ,folder+"/"+imagebase],(err,res)=>{
-    let list=fs.readdirSync(folder)
-   let lista=[]
-    for(let elem of list){
+const multer=require('multer');
+const req = require('express/lib/request');
+const app = express()
+
+const filefolder=function (req,res,next){
+    
+    const tmpfd=uuid.v4();
+    const tempfolder=folder+tmpfd;
+    exec('mount -t tmpfs -o size=100m myramdisk /tmp/ramdisk',()=>{fs.mkdirSync(tempfolder)});
+    res.dest=tempfolder;
+    next();
+}
+
+
+
+//app.use('*',filefolder)
+const port = 3000
+
+
+const folder="/tmp/ramdisk/"
+//const storage = multer.diskStorage({
+//    destination: function (req, res, cb) {
+//      cb(null,res.dest)
+//    },
+//    filename: function (req, res, cb) {
+//      console.log(JSON.stringify(req.body))
+//      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//      const name= uniqueSuffix+'.pdf'
+//      res.fileSavedName=name
+//      cb(null, name)
+//    }
+///  })
+  
+  const upload = multer({ dest: folder })
+
+  app.post('/pdfconvert',upload.single('file'), function (req, res, next) {
+      
+    let originalname=req.file.originalname
+    let filepath=req.file.path
+    const filename=req.file.filename
+    const imagebase=req.file.filename+".png"
+    fs.mkdirSync(filepath+'1')
+    im.convert([filepath,'-colorspace', 'RGB', '-alpha' ,'remove' ,filepath+'1'+"/"+imagebase],(err,res)=>{
+       
+      let list=fs.readdirSync(filepath+'1'+"/")
+       let lista=[]
+        for(let elem of list){
+            
+           lista.push (filepath+'1'+"/"+elem)
+    
+        }
         
-       lista.push (folder+"/"+elem)
+        let instructions=lista.concat([filepath+'1'+'/'+originalname])
+        
+        im.convert(instructions,(err,result)=>{
+          console.log(res)
 
-    }
+        })
     
-    let instructions=lista.concat([folder+'/'+filename])
     
-    im.convert(instructions)
+    })
+    
+    setTimeout(()=>{res.sendFile(filepath+'1'+'/'+originalname)},4000)
+    
+  })
 
 
-})
 
+
+app.listen(port)
+console.log("app listening on port" + port)
 
