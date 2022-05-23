516733b8ad5c1724f7a8e8cd60174df70099cd08
Andrés Caffarena <andrescaffarena@hotmail.com>
2022-05-23 10:32:18 -0300

    dockerizado con manejo de errores

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const im= require('imagemagick');
-
+const CronJob = require('cron').CronJob;
 const express = require('express')
-
+const rimraf=require('rimraf')
 const fs=require("fs")
 const multer=require('multer');
 const req = require('express/lib/request');
@@ -34,11 +34,76 @@ return new Promise((resolve,reject)=>{
 
 const folder="/app/ramdisk/"
 
+const job = new CronJob('0 */10 * * * *', function() {
+	const d = Date.now().toString();
+	console.log("limpieza de archivos temporarios");
+  let lista=fs.readdirSync(folder)
+  for (elem of lista){
+      
+        let out=elem.toString()
+        console.log(out)
+        let splited=out.split("-")[1]
+        console.log(splited)
+        console.log("date " + d)
+
+        if (d- splited >300000){
+
+          if(fs.existsSync(folder+elem)){
+            if(fs.lstatSync(folder+elem).isDirectory()){
+              
+           rimraf(folder+elem,()=>{console.log("eliminado directorio" +folder+elem)})
+          }else{
+
+            fs.rm(folder+elem,()=>{console.log("eliminado archivo" +folder+elem)})
+
+          }
+         }
+
+        }
+
+
+
+  }
+});
+
+job.start();
   
-  const upload = multer({ dest: folder })
+  //const upload = multer({ dest: folder })
+
+
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/app/ramdisk/')
+    },
+    
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({
+    storage: storage,
+    
+    fileFilter: function (req, file, cb) {
+      if (file.mimetype != 'application/pdf') {
+        return cb(null,false)
+      }
+      cb(null, true)
+    },
+    
+    limits:{
+      fileSize: 2048 * 1024
+    } 
+
+  })
 
   app.post('/pdfconvert',upload.single('file'), function (req, res, next) {
-      console.log(req.file)
+      if(req.file==undefined){
+        res.response="bad file" 
+        res.sendStatus(400)
+      }else{
+
     let originalname=req.file.originalname
     let filepath=req.file.path
     const filename=req.file.filename
@@ -60,8 +125,7 @@ const folder="/app/ramdisk/"
         
         asyncIm(instructions).then((result)=>{
           res.sendFile(filepath+'1'+'/'+originalname)
-          fs.rm(filepath+'1',()=>{}),
-          fs.rm(filepath,()=>{})
+          
 
         }).catch((err)=>{console.log(err)
         
@@ -74,7 +138,7 @@ const folder="/app/ramdisk/"
    
   ).catch((err)=>{console.log(err) 
   res.status(500)
-  })})
+  })}})
 
 
 
